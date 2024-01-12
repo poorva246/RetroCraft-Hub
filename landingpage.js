316695a4cc24c9c4435c059c636dab2ab9da93cf
Poorva Sharma <poorvasharma253@gmail.com>
2024-01-12 19:39:03 +0530

    new work

--- landingpage.js
+++ landingpage.js
@@ -2,33 +2,40 @@
     console.log("ok")
     //   let toggle = document.getElementById = "toggle";
       // let submenu = document.querySelector(".submenu");
-    var flag = 0;
+    var flag = 1;
       toggle.addEventListener("click", function () {
         // submenu.classList.toggle('active');
         if(flag%2===1){
     
-          document.body.classList.remove("dark");
-          document.querySelector('header').classList.remove("darkheader")
-          document.querySelector('footer').classList.remove("darkfooter")
+
+
+          document.body.classList.add("dark");
+          document.querySelector('header').classList.add("darkheader")
+          document.querySelector('footer').classList.add("darkfooter")
           document.querySelectorAll('*').forEach(function(element) {
-             element.style.color = 'white';
-          });
-          document.querySelector('nav').classList.remove("darkheader")
-          toggle.classList.remove("darkmode")
-          toggle.innerHTML = "LIGHT MODE"
+           element.style.color = 'white';
+         });
+         toggle.classList.add("darkmode")
+         toggle.innerHTML = "DARK MODE"
     
     
     
        }
        else {
-          document.body.classList.add("dark");
-          document.querySelector('header').classList.add("darkheader")
-          document.querySelector('footer').classList.add("darkfooter")
-          document.querySelectorAll('*').forEach(function(element) {
+
+        document.body.classList.remove("dark");
+        document.querySelector('header').classList.remove("darkheader")
+        document.querySelector('footer').classList.remove("darkfooter")
+        document.querySelectorAll('*').forEach(function(element) {
            element.style.color = 'black';
-         });
-         toggle.classList.add("darkmode")
-         toggle.innerHTML = "DARK MODE"
+        });
+        document.querySelector('nav').classList.remove("darkheader")
+        toggle.classList.remove("darkmode")
+        toggle.innerHTML = "LIGHT MODE"
+
+
+
+
           }
           flag++;
       });
